cd081af02e679364f66ce347792bd9ee1a99f142
Наталья Осокина <amnevsepo84@mail.ru>
2019-05-05 17:28:05 +0300

    Добавляет компонент для отрисовки главной страницы

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/app/app.jsx';
+
+
+const mainContainer = document.querySelector(`#root`);
+
+
+const init = () => {
+
+  ReactDOM.render(
+      <App/>,
+      mainContainer
+  );
+};
+
+init();
